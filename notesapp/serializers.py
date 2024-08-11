ebfa7f2c2fba9f37299ccba67b91c115cb7966e0
godwinarickkat <154876470+godwinarickkat@users.noreply.github.com>
2024-08-11 18:18:17 +0530

    Code Commit

---
+++ notesapp/serializers.py
@@ -0,0 +1,8 @@
+from notesapp.models import Note
+from rest_framework import serializers
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id","title","body","slug","category","created","updated"]
